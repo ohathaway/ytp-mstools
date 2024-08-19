3ddb37e8fbf400f8a35e6c30789eeefa29fff9ec
Owen Hathaway <owen@prsdata.com>
2024-08-19 17:52:07 -0600

    migrated app framed out

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        { src: 'https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js' }
+      ]
+    }
+  },
   buildModules: ['@nuxtjs/svg'],
   compatibilityDate: '2024-04-03',
   css: [
     '@/assets/css/global.css'
   ],
   devtools: { enabled: true },
-  head: {
-    script: [
-      { src: 'https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js' }
-    ]
-  },
   ssr: false,
   vue: {
     compilerOptions: {

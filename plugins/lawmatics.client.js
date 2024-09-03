0c6d93e680541dc41f8bcff47194ad25bdf96457
Owen Hathaway <owen@prsdata.com>
2024-09-03 14:52:09 -0600

    adjusted lawmatics search to property use fuzzy syntax for partial strings

--- plugins/lawmatics.client.js
+++ plugins/lawmatics.client.js
@@ -11,9 +11,13 @@ export default defineNuxtPlugin((nuxtApp) => {
       phone: 'phone'
     }
 
+    let fuzzyTerm = searchTerm.replace(/^\s/, '')
+                              .replace(/\s$/, '')
+    fuzzyTerm = `%${fuzzyTerm}%`
+
     const params = new URLSearchParams({
       filter_field: searchTypes[searchType] || 'last_name',
-      filter_value: searchTerm,
+      filter_value: fuzzyTerm,
       filter_operator: 'ilike',
       fields: 'all'
     })

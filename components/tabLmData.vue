3b93ae305ece6e42976a4ebaff0cb0fa5963b6d4
Owen Hathaway <owen@prsdata.com>
2024-09-03 17:59:27 -0600

    ui usability improvements to lm search

--- components/tabLmData.vue
+++ components/tabLmData.vue
@@ -1,35 +1,29 @@
 <template>
-  <div class="ohl-tab-panel" style="width: 290px;">
-    <div class="lmLogin" v-if="!isLmAuthenticated()">
+  <div class="ohl-tab-panel">
+    <div class="lmLogin" v-if="!isLmAuthenticated">
       <fluent-button>
         Log In to Lawmatics
       </fluent-button>
     </div>
     <div class="lm-data" v-else>
-      <fluent-search appearance="outline" placeholder="Search ..." v-model="searchTerm" />
+      <fluent-search appearance="outline" placeholder="Search ..." v-model="searchTerm" @keyup="handleKeyUp" />
       <fluent-radio-group orientation="horizontal" name="searchType" :value="searchType"
-        @change="event => searchType = event.target.value">
-        <fluent-radio value="last_name">lname</fluent-radio>
-        <fluent-radio value="first_name">fname</fluent-radio>
-        <fluent-radio value="email">email</fluent-radio>
-        <fluent-radio value="phone">phone</fluent-radio>
+        @change="handleSearchTypeChange">
+        <fluent-radio v-for="type in searchTypes" :key="type.value" :value="type.value">
+          {{ type.label }}
+        </fluent-radio>
       </fluent-radio-group>
-      <fluent-button @click="runSearch(lmUrl)" :disabled="!searchTerm">
+      <fluent-button @click="runSearch" :disabled="!searchTerm">
         Search Lawmatics
       </fluent-button>
-      <div v-if="searchHasRun">
-        <fluent-card>
-          <div class='no-results' v-if="searchResults.length === 0">
-            No results found
-          </div>
-          <fluent-accordion aria-labelledby="contactName" style="width: 100%;">
-            <LawmaticsContactAccordionItem
-              v-for="contact in searchResults"
-              :key="contact.id"
-              :contact="contact" />
-          </fluent-accordion>
-        </fluent-card>
-      </div>
+      <fluent-card v-if="searchHasRun">
+        <div class='no-results' v-if="searchResults.length === 0">
+          No results found
+        </div>
+        <fluent-accordion v-else aria-labelledby="contactName" style="width: 100%;">
+          <LawmaticsContactAccordionItem v-for="contact in searchResults" :key="contact.id" :contact="contact" />
+        </fluent-accordion>
+      </fluent-card>
     </div>
   </div>
 </template>
@@ -39,28 +33,57 @@ const { $searchLm, $setSearchUrl } = useNuxtApp()
 
 const searchTerm = ref('')
 const searchType = ref('last_name')
-const lmUrl = ref()
 const searchResults = ref([])
 const searchHasRun = ref(false)
 
-const isLmAuthenticated = () => {
-  return true
+const searchTypes = [
+  { value: 'last_name', label: 'lname' },
+  { value: 'first_name', label: 'fname' },
+  { value: 'email', label: 'email' },
+  { value: 'phone', label: 'phone' },
+]
+
+const lmUrl = computed(() =>
+  $setSearchUrl(searchType.value, searchTerm.value)
+)
+
+// Replace with actual authentication logic
+const isLmAuthenticated = computed(() => true)
+
+const runSearch = async () => {
+  searchHasRun.value = true
+  searchResults.value = await $searchLm(lmUrl.value)
 }
 
-watchPostEffect(() => {
-  lmUrl.value = $setSearchUrl(searchType.value, searchTerm.value)
-})
+const handleKeyUp = (event) => {
+  if (event.key === 'Enter') {
+    runSearch()
+  }
+}
 
-const runSearch = async url => {
-  searchHasRun.value = true
-  const lmResults = await $searchLm(url)
-  searchResults.value = lmResults
+const handleSearchTypeChange = (event) => {
+  searchType.value = event.target.value
+  searchTerm.value = ''  // Reset the search term
+  searchResults.value = []  // Clear previous search results
+  searchHasRun.value = false  // Reset the search status
 }
 </script>
 
 <style scoped>
-.ohl-tab-panel { width: 100%; }
-.lm-data { display: inline-grid; gap: 15px; }
-.contact-item { width: 100%; }
-.no-results { padding: 1rem;}
+.ohl-tab-panel {
+  width: 290px;
+}
+
+.lm-data {
+  display: inline-grid;
+  gap: 15px;
+}
+
+.contact-item {
+  width: 100%;
+}
+
+.no-results {
+  padding: 1rem;
+}
 </style>

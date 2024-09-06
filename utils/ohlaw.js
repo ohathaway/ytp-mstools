07f7727d9cdf60ff6434340622f6f44f70fae4f7
Owen Hathaway <owen@prsdata.com>
2024-09-06 10:33:09 -0600

    re-added stripCountry utility method

--- utils/ohlaw.js
+++ utils/ohlaw.js
@@ -1,3 +1,7 @@
 export const openNewWindow = link => {
   window.open(link, '_blank', 'noopener,noreferrer')
 }
+
+export const stripCountry = address => {
+  return address.replace(/\,\sUnited\sStates.*/, '')
+}

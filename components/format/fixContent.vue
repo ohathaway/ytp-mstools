7762ed6cbb46d6814c802edceec37d0c63b66983
Owen Hathaway <owen@prsdata.com>
2024-10-07 17:50:48 -0600

    working on fixing paragraph content

--- components/format/fixContent.vue
+++ components/format/fixContent.vue
@@ -11,9 +11,9 @@ const removeEmptyParagraphs = async () => {
     await paragraphs.load('items')
     await context.sync()
 
-    paragraphs.map(para => {
-      console.debug('paragraph: ', para)
-    })
+    // paragraphs.map(para => {
+      console.debug('paragraph: ', paragraphs)
+    // })
 
   })
 }
